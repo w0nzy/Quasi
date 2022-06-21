0a4330ad8c52c2a06b3c4ffc5ac08310fafbf74d
Alperen Çavuş <105172848+w0nzy@users.noreply.github.com>
2022-06-21 15:48:21 +0300

    Delete th0rn-beta directory

--- th0rn-beta/main.py
+++
@@ -1,7 +0,0 @@
-from core.base import Bootloader
-from core.utils.printing import print_failure
-try:
-    with Bootloader() as bootloader:
-        bootloader.boot()
-except Exception as loader_err:
-    print_failure("Hata boot edilirken bir hata oluştu",loader_err)
